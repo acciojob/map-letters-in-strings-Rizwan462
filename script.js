8552231356ce63355aba1c87065daeb0697ae70c
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-01-15 14:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function letterMap(word){
+	const letter = {};
+
+for(let i = 0; i<word.length; i++){
+	const letterWord = word[i];
+
+	if(!(letterWord in letter)){
+		letter[letterWord] = [];
+	}
+	letterMap[letterWord].push(i);
+}
+	return letter
+}
+console.log(mapLetters("dodo"));   
+console.log(mapLetters("froggy")); 
+console.log(mapLetters("grapes"));
